a76c4928c8ba88c10d32fb4c09a938cf8734408c
Aditya kumar kanouje <aditya>
2021-08-28 10:14:49 +0530

    TEMPORARY COMMIT

--- scrapping.py
+++ scrapping.py
@@ -1,8 +1,32 @@
 import sys
 import os
-import requests, re, time, mysql.connector
+from time import sleep
+import requests, re, datetime, mysql.connector
 from bs4 import BeautifulSoup
-from justbackoff import Backoff
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+
+jobStore = {
+    "default": SQLAlchemyJobStore(url='mysql+pymysql://scrapper:scrappy@localhost:3306/webpage_data?charset=utf8')
+}
+
+# scheduler = BackgroundScheduler({
+#     'apscheduler.jobstores.default': {
+#         'type': 'sqlalchemy',
+#         'url': 'mysql://scrapper:scrappy@localhost:3306/scrapper_jobs?charset=utf8'
+#     }
+# })
+
+scheduler = BackgroundScheduler({
+    'apscheduler.jobstores.default': {
+        'type': 'sqlalchemy',
+        'url': 'mysql+pymysql://scrapper:scrappy@localhost:3306/webpage_data?charset=utf8'
+    }
+})
+
+# scheduler.configure(jobStore=jobStore)
+scheduler.start()
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -12,65 +36,54 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
+Attributes = ['description','Description','keywords','keyword','Keywords','Keyword']
 
-def crawling_indexing(URL):
-    Attributes = ['description','Description','keywords','keyword','Keywords','Keyword']
-    parsed_links = set()
-    #entry = {}
-    #collected_data = []
-    link_stack = []
-    link_stack.append(URL)
-    while len(link_stack) != 0:   # traversing all the links
-        #try:
-        #    r = requests.get(link_stack[0])
-        #except Exception as e:
-        #    print("could not load page ")
-        currentUrl = link_stack[0]
-        r = requests.get(currentUrl)
-        print('{}: {}'.format(r.status_code, currentUrl))
-        #b = Backoff(min_ms=300000, max_ms=3600000, factor=2, jitter=False)
-        if r.status_code == 200:
-            c = r.content
-            parsed_content = BeautifulSoup(c,"html.parser")
-            meta_list  = parsed_content.find_all("meta")
-            for meta in meta_list:
-                if 'name' in meta.attrs:
-                    name = meta.attrs['name']
-                    if name in Attributes:
-                        #entry = {}
-                        #entry[name.lower()] = meta.attrs['content']
-                        #print(entry)
+parsedLinks = []
+
+def parseLink(URL):
+    r = requests.get(URL)
+    print('[{}]: {} - {}'.format(datetime.datetime.now(), r.status_code, URL))
+    if r.status_code == 200:
+        parsedLinks.append(URL)
+
+        #add_to_db(link, URL)
+
+        c = r.content
+        parsed_content = BeautifulSoup(c,"html.parser")
+        meta_list  = parsed_content.find_all("meta")
+
+        for meta in meta_list:
+            if 'name' in meta.attrs:
+                name = meta.attrs['name']
+                if name in Attributes:
+                    #Extracting description and keywords from meta tags and adding to database 
                         #Extracting description and keywords from meta tags and adding to database 
-                        if len(meta.attrs['content']) >= 1:
-                            collected_data = []
-                            collected_data.append(meta.attrs['content'])
-                            #Storing collected link and data to database
-                            add_to_db(currentUrl, 'adw', 'Abc')
-                            collected_data.clear()
-                        else:
-                            print('could not find all required attributes for URl')
-            #if len(entry) >= 1:
-            #    if entry not in collected_data:
-            #        collected_data.append(entry)
-                #print(collected_data)
-           #else:
-           #    print('could not find all required attributes for URL ')
-            
-            for link in parsed_content.find_all('a',attrs = {'href': re.compile("^https://")}):
-                if link.get('href') not in link_stack and link.get('href') not in parsed_links:
-                    link_stack.append(link.get('href'))
-            
-            parsed_links.add(link_stack.pop(0))
-            #time.sleep(15) # Halting the loop for 15 second
+                    #Extracting description and keywords from meta tags and adding to database 
+                        #Extracting description and keywords from meta tags and adding to database 
+                    #Extracting description and keywords from meta tags and adding to database 
+                    if len(meta.attrs['content']) >= 1:
+                        collected_data = []
+                        collected_data.append(meta.attrs['content'])
+                        
+                        #Storing collected link and data to database
+                        add_to_db(URL, 'adw', 'Abc')
+                        collected_data.clear()
+                    else:
+                        print('could not find all required attributes for URl')
 
-            #for link in parsed_content.find_all('a'):
-            #    if (link != None and link != '#') and link not in link_stack:
-                #if link.startswith('http') == True or link.startswith('https') == True:
-            #        link_stack.append(link.get('href'))
-            #        print(link_stack)
-        
-        else:
-            link_stack.append(currentUrl)
+        linkArray = parsed_content.find_all('a', attrs = {'href': re.compile("^https://")})
+
+        for idx, link in enumerate(linkArray):
+            if link.get('href') and link.get('href') not in parsedLinks:
+                nextExecTime = datetime.datetime.now() + datetime.timedelta(0, 3 * (idx+1))
+                # print('Next execution: {} - {}'.format(nextExecTime, link.get('href')))
+                #scheduler.add_job(parseLink, nextExecTime, [link.get('href')])
+                scheduler.add_job(parseLink, run_date = nextExecTime, args=[link.get('href')])
+    else:
+        nextExecTime = datetime.datetime.now() + datetime.timedelta(0, 60)
+        # print('Next execution: {} - {}'.format(nextExecTime, link.get('href')))
+        # scheduler.add_job(parseLink, nextExecTime, [URL])
+        scheduler.add_job(parseLink, run_date=nextExecTime, args=[URL])
 
 
 def add_to_db(link, description, keywords):
@@ -81,10 +94,13 @@ def add_to_db(link, description, keywords):
 
 if __name__ == '__main__':
     try:
-        crawling_indexing('https://www.geeksforgeeks.org/')
-    except KeyboardInterrupt:
-        try:
-            mydb.commit()
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+        if scheduler.get_jobs() is None:
+            parseLink('https://www.geeksforgeeks.org/')
+
+        while True:
+            sleep(2)
+            if scheduler.get_jobs() is None:
+                break
+    except:
+        scheduler.shutdown()
+        # mydb.commit()
--- testing.py
+++ testing.py
@@ -1,12 +1,22 @@
-from  justbackoff import Backoff
+#from  justbackoff import Backoff
 
-b = Backoff(min_ms=300000, max_ms=3600000, factor=2, jitter=False)
-print(b.duration())
-print(b.duration())
-print(b.duration())
+#b = Backoff(min_ms=300000, max_ms=3600000, factor=2, jitter=False)
+#print(b.duration())
+#print(b.duration())
+#print(b.duration())
 
 #a = 4
 #while a>=0:
 #    if a%2 == 0:
 #        print(b.duration())
 #    a-=1
+
+import datetime
+exec_date = datetime.datetime.now()
+print(exec_date, end=' ')
+timeDelta = datetime.timedelta(0, 10)
+newTime = exec_date + timeDelta
+print(newTime)
+print(type(exec_date))
+print(type(newTime))
+print(type(datetime.datetime(2009, 11, 6, 16, 30, 5)))
